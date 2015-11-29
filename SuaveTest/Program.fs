226a9fe9251f81fd19f374ab7475efc400b6fcc7
rastapanda <me@jspenc.com>
2015-11-29 18:23:36 +0000

    hello world example

---
+++ SuaveTest/Program.fs
@@ -0,0 +1,6 @@
+﻿open Suave                 // always open suave
+open Suave.Http.Successful // for OK-result
+open Suave.Web             // for config
+
+//starts a web server returning a 200 ok on localhost:8083 
+startWebServer defaultConfig (OK "Hello World!")
