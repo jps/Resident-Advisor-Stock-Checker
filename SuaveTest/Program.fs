f6be1acf87bfb1e80ddb301da08a82cc9f39aa08
rastapanda <me@jspenc.com>
2015-11-29 18:44:25 +0000

    simple pipes example

--- SuaveTest/Program.fs
+++ SuaveTest/Program.fs
@@ -1,6 +1,21 @@
-﻿open Suave                 // always open suave
-open Suave.Http.Successful // for OK-result
-open Suave.Web             // for config
+﻿open Suave
+open Suave.Http
+open Suave.Http.Applicatives
+open Suave.Http.Successful
+open Suave.Web
 
 //starts a web server returning a 200 ok on localhost:8083 
-startWebServer defaultConfig (OK "Hello World!")
+//startWebServer defaultConfig (OK "Hello World!")
+let app = 
+    choose
+        [ GET >>= choose
+            [  path "/" >>= OK "Hello World!"
+               path "/hello" >>= OK "Hello GET"
+               path "/goodbye" >>= OK "Goodbye GET"]
+          POST >>= choose
+            [   path "/hello" >>= OK "Hello POST"
+                path "/goodbye" >>= OK "Goodbye POST"]
+        ]
+
+//starts a web server on localhost:8083 
+startWebServer defaultConfig (app)
